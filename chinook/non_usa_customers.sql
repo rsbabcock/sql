86168f79f602c576d2dbfd48264d4c787ec5162e
rsbabcock <rachael.s.babcock@gmail.com>
2018-08-01 12:29:39 -0500

    added non usa customers query

---
+++ chinook/non_usa_customers.sql
@@ -0,0 +1,4 @@
+--  Provide a query showing Customers (just their full names, customer ID and country) who are not in the US.
+SELECT c.FirstName, c.LastName, c.CustomerId, c.Country
+from Customer c
+where c.Country != "USA"
